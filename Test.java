bcc7f8b0bcaa2e0433495f0f7296d21749290801
Flames Co. LLC <84363801+FlamesCo@users.noreply.github.com>
2024-02-05 14:42:01 -0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+// This is a simple Java program that prints "Hello, Cat!" to the console.
+public class Test {
+    public static void main(String[] args) {
+        // Print "Hello, Cat!" to the console
+        System.out.println("Hello, Cat!");
+    }
+}
